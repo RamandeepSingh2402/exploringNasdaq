52654a16e594f7e7a559f8ea5fb84a6a40eb26cb
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-08-12 22:25:59 -0300

    made the changes in UI and also used another API for OPEC oil prices

--- exploring-nasdaq/src/components/Homepage.js
+++ exploring-nasdaq/src/components/Homepage.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "../CSS/HomePage.css"
+import videobg from '../Media/Data_Grid.mp4';
+import DownloadCSV from "./DownloadCSV";
 
-function downloadCSV(props) { 
+function downloadOilCSV() {
+    const oilURL = "https://data.nasdaq.com/api/v3/datasets/OPEC/ORB.csv";
+    var link = document.createElement('a');
+    link.href = oilURL;
+    link.download = 'oilData.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function downloadAppleCSV(props) { 
     const downloadCSVUrl = "https://data.nasdaq.com/api/v3/datasets/WIKI/AAPL.csv";
     const link = document.createElement('a');
     link.href = downloadCSVUrl;
@@ -9,25 +21,39 @@ function downloadCSV(props) {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    
 }
+
 
 function HomePage () {
     return (
-        <div id="homePage">
-            <video autoPlay muted loop id="stock-video">
-                <source src="">
-                        
-                </source>
-            </video>
             <div>
-                Welcome to NASDAQ Explorer!
+                <div id="backgroundVideo">
+                    <video src={videobg} autoPlay loop muted />
+                </div>
+                <div id="content">
+                    <h1>
+                        Welcome to NASDAQ Explorer!
+                    </h1>
+                    <div id="stocks">
+                        <div id="appleStocks">
+                            <p> 
+                                Interested in Apples Stock Prices? <br /> Click the button below to download the CSV file with Apple prices since 1980
+                            </p>
+                            <button id="downloadApple" onClick={downloadAppleCSV}>
+                                APPL's
+                            </button>
+                        </div>
+                        <div id="opecOil">
+                            <p> 
+                                Interested in OPEC oil prices since 2003? <br /> Click the button below to download the CSV file with OPEC oil prices since 2003
+                            </p>
+                            <button id='opecOilPrices' onClick={downloadOilCSV}>
+                                OPEC Oil
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <button id="downloadApple" onClick={downloadCSV}>
-                APPL's stock prices since 1980!
-            </button>
-        </div>
     );
 }
 
